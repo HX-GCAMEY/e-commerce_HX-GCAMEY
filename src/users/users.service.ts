54055fb5b356d5ca0171caf7e3fa31c057863d13
Gama <lic.camey@gmail.com>
2024-03-12 17:23:02 +0100

    solutions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,23 @@ import { UsersRepository } from './users.repository';
 @Injectable()
 export class UsersService {
   constructor(private UsersRepository: UsersRepository) {}
-  getUsers() {
-    return this.UsersRepository.getUsers();
+  getUsers(page: number, limit: number) {
+    return this.UsersRepository.getUsers(page, limit);
+  }
+
+  getUser(id: number) {
+    return this.UsersRepository.getUser(id);
+  }
+
+  addUser(user: any) {
+    return this.UsersRepository.addUser(user);
+  }
+
+  updateUser(id: number, user: any) {
+    return this.UsersRepository.updateUser(id, user);
+  }
+
+  deleteUser(id: number) {
+    return this.UsersRepository.deleteUser(id);
   }
 }
